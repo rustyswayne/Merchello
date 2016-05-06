7b3cbb32a8bf13ffcd4df160890ef18cdd6c938e
rs@merchello.com <rs@merchello.com>
2016-05-05 17:40:03 -0700

    [WIP] Concept for UI Component model in the .Web

---
+++ src/Merchello.Web/Models/Ui/Components/IBasketModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Merchello.Web.Models.Ui.Components
+{
+    /// <summary>
+    /// Defines a basket UI component.
+    /// </summary>
+    public interface IBasketModel : IStoreComponent
+    {
+        /// <summary>
+        /// Gets or sets the checkout page url.
+        /// </summary>
+        string CheckoutPageUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets the continue shopping url.
+        /// </summary>
+        string ContinueShoppingUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets the basket items.
+        /// </summary>
+        IBasketItemModel[] Items { get; set; } 
+    }
+}
---
+++ src/Merchello.Web/Models/Ui/Components/IStoreComponent.cs
@@ -0,0 +1,9 @@
+﻿namespace Merchello.Web.Models.Ui.Components
+{
+    /// <summary>
+    /// Marker interface for Merchello Stores UI Components.
+    /// </summary>
+    public interface IStoreComponent
+    {
+    }
+}
